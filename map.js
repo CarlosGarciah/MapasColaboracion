e8552eb0d1555cc812ad5c054d52ba9a93521413
Carlos <carlosgarcia21pinos@gmail.com>
2017-10-02 18:51:40 +0200

    Marcadores arreglados e interracion desactivada

--- map.js
+++ map.js
@@ -18,7 +18,8 @@ var catastroBase = L.tileLayer.wms('http://ovc.catastro.meh.es/Cartografia/WMS/S
 
 var bar1 = L.marker([37.1916462, -3.711131]).bindPopup('Bar d`tapas'),
     bar2    = L.marker([37.1915012, -3.7118028]).bindPopup('Bar santi'),
-    bar3    = L.marker([37.1899571, -3.7166183]).bindPopup('Taberna el arco'),
+    bar3    = L.marker([37.1899571, -3.7166183]).bindPopup('Taberna el arco');
+
 var bares = L.layerGroup([bar1, bar2, bar3]);
 
 
@@ -37,3 +38,12 @@ L.control.layers(baseMaps, overlayMaps,{
 	position: 'topright',
 	collapsed: false
 }).addTo(map);
+
+map.dragging.disable();
+map.touchZoom.disable();
+map.doubleClickZoom.disable();
+map.scrollWheelZoom.disable();
+map.boxZoom.disable();
+map.keyboard.disable();
+if (map.tap) map.tap.disable();
+document.getElementById('map').style.cursor='default';
