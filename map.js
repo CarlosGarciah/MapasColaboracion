bf9617691783810c5b4fbadd3eb88698ba9df4c8
Juan Antonio Morilla <juanjamfw@gmail.com>
2017-10-02 19:52:49 +0200

    Marcadores de transporte preparados

--- map.js
+++ map.js
@@ -17,21 +17,34 @@ var catastroBase = L.tileLayer.wms('http://ovc.catastro.meh.es/Cartografia/WMS/S
 
 
 var bar1 = L.marker([37.1916462, -3.711131]).bindPopup('Bar d`tapas'),
-    bar2    = L.marker([37.1915012, -3.7118028]).bindPopup('Bar santi'),
-    bar3    = L.marker([37.1899571, -3.7166183]).bindPopup('Taberna el arco');
+    bar2 = L.marker([37.1915012, -3.7118028]).bindPopup('Bar santi'),
+    bar3 = L.marker([37.1899571, -3.7166183]).bindPopup('Taberna el arco');
+//    bar4 = L.marker([37., -3.]).bindPopup('');
+//    bar5 = L.marker([37., -3.]).bindPopup('');
+
+var bus1 = L.marker([37., -3.]).bindPopup(''),
+var bus2 = L.marker([37., -3.]).bindPopup(''),
+var bus3 = L.marker([37., -3.]).bindPopup(''),
+var bus4 = L.marker([37., -3.]).bindPopup(''),
+var bus5 = L.marker([37., -3.]).bindPopup(''),
+var bus6 = L.marker([37., -3.]).bindPopup(''),
+var bus7 = L.marker([37., -3.]).bindPopup(''),
 
 var bares = L.layerGroup([bar1, bar2, bar3]);
 
 
 
 var baseMaps = {
-    "OSM": osmBase,
+    "Santa Fe": osmBase,
     "Catastro": catastroBase
 };
 
 var overlayMaps = {
     "Bares": bares,
 
+};
+var overlayMaps = {
+    "Transporte": transporte,
 };
 
 L.control.layers(baseMaps, overlayMaps,{
